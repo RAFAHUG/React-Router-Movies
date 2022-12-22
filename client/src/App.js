521916efd7e32738c07390a4858b6e9712843834
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2022-12-22 14:02:00 -0600

    use of react router and react routers component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import axios from 'axios';
-
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -12,8 +14,8 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+          console.log(response.data); // Study this response with a breakpoint or log statements
+          setMovieList(response.data);// and set the response data as the 'movieList' slice of state
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +32,15 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      
+      <Route exact path='/movies/:id'>
+          <Movie />
+        </Route>
+
+      <Route path='/'>
+          <MovieList movies={movieList} />
+      </Route>
+
     </div>
   );
 }
